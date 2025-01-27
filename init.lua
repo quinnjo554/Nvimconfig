a9b1647dd72bfe0ef27303399b1093210e44fa59
quinn <quinnjohnson24@gmail.com>
2025-01-27 12:41:57 -0600

    init

---
+++ init.lua
@@ -0,0 +1,56 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.g.term_buf = 0
+vim.g.term_win = 0
+vim.cmd([[
+  function! TermToggle(height)
+    if win_gotoid(g:term_win)
+        hide
+    else
+        botright new
+        exec "resize " . a:height
+        try
+            exec "buffer " . g:term_buf
+        catch
+            call termopen($SHELL, {"detach": 0})
+            let g:term_buf = bufnr("")
+            set nonumber
+            set norelativenumber
+            set signcolumn=no
+        endtry
+        startinsert!
+        let g:term_win = win_getid()
+    endif
+  endfunction
+]])
+
+local function set_transparency()
+  vim.cmd([[
+hi Normal guibg=NONE ctermbg=NONE
+hi NormalNC guibg=NONE ctermbg=NONE
+hi SignColumn guibg=NONE ctermbg=NONE
+hi StatusLine guibg=NONE ctermbg=NONE
+hi StatusLineNC guibg=NONE ctermbg=NONE
+hi VertSplit guibg=NONE ctermbg=NONE
+hi TabLine guibg=NONE ctermbg=NONE
+hi TabLineFill guibg=NONE ctermbg=NONE
+hi TabLineSel guibg=NONE ctermbg=NONE
+hi Pmenu guibg=NONE ctermbg=NONE
+hi PmenuSel guibg=NONE ctermbg=NONE
+hi NeoTreeNormal guibg=NONE ctermbg=NONE
+hi NeoTreeNormalNC guibg=NONE ctermbg=NONE
+hi NeoTreeWinSeparator guibg=NONE ctermbg=NONE
+hi NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+hi EndOfBuffer guibg=NONE ctermbg=NONE
+]])
+end
+
+-- Apply transparency settings initially
+set_transparency()
+
+-- Reapply transparency on buffer enter
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = set_transparency,
+})
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.api.nvim_set_keymap("i", "<Tab>", 'pumvisible() ? coc#_select_confirm() : "<Tab>"', { expr = true })
+
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true })
+vim.api.nvim_set_keymap("v", "<Space>", "<Esc>", { noremap = true })
+
+--For Terminal
+vim.api.nvim_set_keymap("n", "<A-t>", ":call TermToggle(12)<CR>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<A-t>", "<Esc>:call TermToggle(12)<CR>", { noremap = true })
+vim.api.nvim_set_keymap("t", "<A-t>", "<C-\\><C-n>:call TermToggle(12)<CR>", { noremap = true })
+
+vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+vim.api.nvim_set_keymap("t", ":q!", "<C-\\><C-n>:q!<CR>", { noremap = true })
+
+--Ctr click into
+vim.api.nvim_set_keymap("n", "zm", "<Cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
